36a61cbff753cc7e5ae293ead61604f1a705f2bf
IChris <ivanchristopherea@gmail.com>
2023-09-26 18:37:00 +0700

    Membuat Leaderboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,13 +14,28 @@ class DashboardController extends Controller
         $bulanini = date("m") * 1; //1 atau Januari
         $tahunini = date("Y"); //2023
         $nik = Auth::guard('karyawan')->user()->nik;
-        $presensihariini = DB::table('presensi')->where('karyawan_nik', $nik)->where('tgl_presensi', $hariini)->first();
-        $historibulanini = DB::table('presensi')->whereRaw('MONTH(tgl_presensi)="' . $bulanini . '"')
+        $presensihariini = DB::table('presensi')->where('nik', $nik)->where('tgl_presensi', $hariini)->first();
+        $historibulanini = DB::table('presensi')
+            ->where('nik',$nik)
+            ->whereRaw('MONTH(tgl_presensi)="' . $bulanini . '"')
             ->whereRaw('YEAR(tgl_presensi)="' . $tahunini . '"')
             ->orderBy('tgl_presensi')
             ->get();
+    
+        $rekappresensi = DB::table('presensi')
+            ->selectRaw('COUNT(nik) as jmlHadir, SUM(IF(jam_in > "07:00",1,0)) as jmlTerlambat')
+            ->where('nik', $nik)
+            ->whereRaw('MONTH(tgl_presensi)="' . $bulanini . '"')
+            ->whereRaw('YEAR(tgl_presensi)="' . $tahunini . '"')
+            ->first();
+
+        $leaderboard = DB::table('presensi')
+            ->join('karyawan','presensi.nik', '=', 'karyawan.nik')
+            ->where('tgl_presensi', $hariini)
+            ->orderBy('jam_in')
+            ->get();
 
         $namabulan = ["", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
-        return view('dashboard.dashboard', compact('presensihariini', 'historibulanini', 'namabulan', 'bulanini', 'tahunini'));
+        return view('dashboard.dashboard', compact('presensihariini', 'historibulanini', 'namabulan', 'bulanini', 'tahunini', 'rekappresensi', 'leaderboard'));
     }
 }
