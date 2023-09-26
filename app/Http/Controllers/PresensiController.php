7a324795f573b36988c34e622beae5c318f0dcbf
Alvian Dwi Sanjaya <76217781+AlvianSanjaya@users.noreply.github.com>
2023-09-27 01:14:32 +0700

    membuat fungsi histori presensi

--- app/Http/Controllers/PresensiController.php
+++ app/Http/Controllers/PresensiController.php
@@ -152,4 +152,23 @@ class PresensiController extends Controller
             return Redirect::back()->with(['error'=>'Data Gagal Diupdate']);;
         }
     }
+    public function histori(){
+        $namabulan = ["","Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
+        return view('presensi.histori', compact('namabulan'));
+    }
+
+    public function gethistori(Request $request){
+        $bulan = $request->bulan;
+        $tahun = $request->tahun;
+        $nik = Auth::guard('karyawan')->user()->nik;
+
+        $histori = DB::table('presensi')
+            ->whereRaw('MONTH(tgl_presensi)="' . $bulan . '"')
+            ->whereRaw('YEAR(tgl_presensi)="' . $tahun . '"')
+            ->where('nik', $nik)
+            ->orderBy('tgl_presensi')
+            ->get();
+
+        return view('presensi.gethistori', compact('histori'));
+    }
 }
