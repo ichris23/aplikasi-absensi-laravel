292b483bbc7e1cc91a2c22a84927552945579dfd
adilsandy <adilsandi10@gmail.com>
2023-09-28 14:13:47 +0700

    Ket Izin

--- app/Http/Controllers/PresensiController.php
+++ app/Http/Controllers/PresensiController.php
@@ -176,7 +176,9 @@ class PresensiController extends Controller
 
     public function izin()
     {
-        return view('presensi.izin');
+        $nik = Auth::guard('karyawan')->user()->nik;
+        $dataizin = DB::table('pengajuan_izin')->where('nik', $nik)->get();
+        return view('presensi.izin', compact('dataizin'));
     }
 
     public function buatizin()
--- resources/views/presensi/izin.blade.php
+++ resources/views/presensi/izin.blade.php
@@ -30,6 +30,31 @@
     @endif
   </div>
 </div>
+<div class="row">
+  <div class="col">
+    @foreach ($dataizin as $d)
+    <ul class="listview image-listview">
+      <li>
+        <div class="item">
+          <div class="in">
+            <div>
+              <b>{{ date("d-m-Y", strtotime($d->tgl_izin)) }} ({{ $d->status== "s" ? "Sakit" : "Izin" }})</b><br>
+              <small class="text-muted">{{ $d->keterangan }}</small>
+            </div>
+            @if ($d->status_approved == 0)
+            <span class="badge bg-warning">Waiting</span>
+            @elseif($d->status_approved == 1)
+            <span class="badge bg-success">Approved</span>
+            @elseif($d->status_approved == 2)
+            <span class="badge bg-danger">Decline</span>
+            @endif
+          </div>
+        </div>
+      </li>
+    </ul>
+    @endforeach
+  </div>
+</div>
 <div class="fab-button bottom-right" style="margin-bottom: 70px;">
   <a href="/presensi/buatizin" class="fab">
     <ion-icon name="add-outline"></ion-icon>
